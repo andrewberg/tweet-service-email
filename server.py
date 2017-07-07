d46277478e4ed84693d59622766dd3c823f186de
Andrew Berg <andrewberg@pobox.com>
2017-07-07 23:31:39 +0000

    Added API endpoints for retweets and noretweets

--- server.py
+++ server.py
@@ -3,13 +3,26 @@ app = Flask(__name__)
 
 from functions import *
 
-@app.route("/tweet-send", methods = ['POST', 'GET'])
-def call():
+@app.route("/tweet-send-nort", methods = ['POST', 'GET'])
+def callNoRt():
 	retval = False
 
 	if request.method == 'POST':
 		tid = request.form['tweet-id']
 		retval = send_tweet_mail(tid, False) # No retweets, "FALSE"
+
+	if retval:
+		return "SUCCESS"
+	else:
+		return "FAILURE - Possible retweet"
+
+@app.route("/tweet-send-rt", methods = ['POST', 'GET'])
+def callRt():
+	retval = False
+
+	if request.method == 'POST':
+		tid = request.form['tweet-id']
+		retval = send_tweet_mail(tid, True) # Retweets, "TRUE"
 
 	if retval:
 		return "SUCCESS"
